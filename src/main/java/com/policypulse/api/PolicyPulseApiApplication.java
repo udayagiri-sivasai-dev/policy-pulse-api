8b093ced6356a03581e80bb10b7c3452c5e711b4
Siva Sai Udayagiri <udayagirishivasai@gmail.com>
2026-01-22 21:05:08 -0500

    Initial commit

---
+++ src/main/java/com/policypulse/api/PolicyPulseApiApplication.java
@@ -0,0 +1,13 @@
+package com.policypulse.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyPulseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyPulseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/policypulse/api/PolicyPulseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.policypulse.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyPulseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
